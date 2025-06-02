e05cbdb172c967f2954ef9fed1884f717e027846
Batubenga0B74 <freudymena84@gmail.com>
2025-06-03 09:47:03 +1100

    add de test header

--- cypress/cypress/e2e/1-getting-started/header.cy.js
+++ cypress/cypress/e2e/1-getting-started/header.cy.js
@@ -8,5 +8,8 @@ describe('Header', () => {
 
   it('deve exibir a logo no header', () => {
     cy.get('img[alt="logo"]').should('be.visible');
+    cy.get('li').first().should('have.text', 'início');
+    cy.get('li').eq(1).should('have.text','Benefícios')
+    cy.get('li').eq(2).should('have.text','FAQ')
   });
 });
